f8c3003a1bc0f2b4b53dc55499ac5f1d4ee34634
mcortezmodell <margarita@cortez-modell.com>
2021-04-28 15:39:14 -0700

    create index.html

--- trackerApp/__init__.py
+++ trackerApp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 from flask_admin import Admin
 
 
@@ -8,11 +8,13 @@ app = Flask(__name__)
 app.config['FLASK_ADMIN_SWATCH'] = 'journal'
 
 # initialize an empty admin interface 
-admin = Admin(app, name='microblog', template_mode='bootstrap3')
+admin = Admin(app, name='Bug Tracker App', template_mode='bootstrap3')
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+# add modelviews for admin interface via. sqlalchemy - use MySQL or PostgresSQL 
+
+@app.route('/')
+def index():
+     return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
