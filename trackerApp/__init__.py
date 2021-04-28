fa3f404a519224ffbd74f161f9b81e607cf6b95b
mcortezmodell <margarita@cortez-modell.com>
2021-04-27 17:52:39 -0700

    initialize empty admin interface

--- trackerApp/__init__.py
+++ trackerApp/__init__.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from flask_admin import Admin
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# set bootswatch theme
+app.config['FLASK_ADMIN_SWATCH'] = 'journal'
+
+# initialize an empty admin interface 
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run()
