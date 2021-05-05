c27b15d2c8473a5dddf7afb3a278e9e0376fe077
mcortezmodell <margarita@cortez-modell.com>
2021-05-04 17:27:58 -0700

    initialize admin interfaces via. myview

--- trackerApp/__init__.py
+++ trackerApp/__init__.py
@@ -1,23 +1,32 @@
-from flask import Flask, session, redirect, url_for, render_template, request
-from flask_admin import Admin
+from flask import Flask, redirect, render_template, request, url_for
+from flask_admin import Admin, BaseView, expose
 
+class MyView(BaseView):
+    @expose('/')
+    def index(self):
+        return self.render('index.html')
 
 app = Flask(__name__)
 
+# initialize admin interface views 
+admin = Admin(app)
+# Add administrative views here
+admin.add_view(MyView(name='Admin', endpoint='test1', category='Manage'))
+admin.add_view(MyView(name='Developers', endpoint='test2', category='Manage'))
+admin.add_view(MyView(name='Switch to User View', endpoint='test3', category='Manage'))
+
+# consider switching to modelviews for admin interface via. sqlalchemy - use MySQL or PostgresSQL 
+
 # set up secret key (move secure info to gitignore file later on)
-app.secret_key = 'pass01234567'
+# app.secret_key = 'pass01234567'
 
-# # set bootswatch theme
-# app.config['FLASK_ADMIN_SWATCH'] = 'journal'
-
-# # initialize an empty admin interface 
-# admin = Admin(app, name='Bug Tracker App', template_mode='bootstrap3')
-
-# add modelviews for admin interface via. sqlalchemy - use MySQL or PostgresSQL 
-
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def index():
      return render_template("index.html")
+
+@app.route('/login', methods=["POST", "GET"])
+def login():
+     return render_template("login.html")
 
 if __name__ == "__main__":
     app.run()
---
+++ trackerApp/auth.py
@@ -0,0 +1,1 @@
+# add flask_admin code here
