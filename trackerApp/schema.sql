d108db13a659db44c0ba5aa2b970b1e3bc6660f3
mcortezmodell <margarita@cortez-modell.com>
2021-05-18 15:19:53 -0700

    build schema

---
+++ trackerApp/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS admn_roles; 
+DROP TABLE IF EXISTS user; 
+DROP TABLE IF EXISTS tickets; 
+
+CREATE TABLE admn_roles (
+    pass
+)
+
+CREATE TABLE user (
+    pass
+)
+
+CREATE TABLE ticket (
+    pass
+)
